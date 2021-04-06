1a8b42b1d489cac9d152ba5005b1d3a867ca0f35
Ewan Douglas <douglase@users.noreply.github.com>
2021-04-06 12:17:39 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ ORIGINAL_WD=$(pwd)
 
 # download and install doorstop
 
-git clone  --branch develop https://github.com/douglase/doorstop
+git clone  --branch develop https://github.com/douglase/doorstop doorstop_lib
 
-cd doorstop
+cd doorstop_lib
 
 python setup.py install
 
