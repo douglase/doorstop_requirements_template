39efca808a31b73db402c30ae96d15f031c1a578
Ewan Douglas <douglase@users.noreply.github.com>
2021-04-05 22:10:03 -0700

    Update generate.bash

--- generate.bash
+++ generate.bash
@@ -46,10 +46,11 @@ tar -czvf vtex.tar.gz ${TEXDIR}
 # download and install doorstop
 git clone https://github.com/douglase/doorstop
 
-cd doorstop
 
 python3 setup.py install
 
-cd ..
+cd ${ORIGINAL_WD}
+
+pwd 
 
 ./doorstop_sync
